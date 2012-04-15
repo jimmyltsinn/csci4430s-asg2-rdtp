beb99f8c0093ed2a51a50a3d7d4979f1add12665
Jimmy Sinn <jimmysinn91@gmail.com>
2012-04-15 16:11:00 +0800

    Testing on multiple packet file

---
+++ filegen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+    int filesize, i;
+    FILE *fd;
+    
+    if (argc != 3 && argc != 4) {
+        printf("Usage:\t%s filename packet_num [byte]\n", argv[0]);
+        return -1;
+    }
+    
+    filesize = atoi(argv[2]) * 1000 + (argc == 4 ? atoi(argv[3]) : 0);
+
+    fd = fopen(argv[1], "w");
+      
+    perror("fopen()");
+
+    for (i = 0; i < filesize; ++i) {
+        char tmp;
+        if (i % 128 == 0) tmp = '\n';
+        else tmp = rand() % 26 + 'a';
+        fwrite(&tmp, 1, 1, fd);
+    }
+    
+    fclose(fd);
+
+    return 0;
+}
